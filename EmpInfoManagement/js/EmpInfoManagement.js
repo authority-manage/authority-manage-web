dbbf4fc2fbb47c9888e69358587992af84df89a1
苞米地教父 <yugongzhou@foxmail.com>
2019-09-25 09:49:07 +0800

    再次修改了员工表的分页

--- EmpInfoManagement/js/EmpInfoManagement.js
+++ EmpInfoManagement/js/EmpInfoManagement.js
@@ -238,7 +238,8 @@ var info = {
 var dateFormat = function(time) {
 	var date = new Date(time);
 	var year = date.getFullYear();
-	var month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
+	var month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;919,
+	
 	var day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
 	return year + "-" + month + "-" + day;
 }
